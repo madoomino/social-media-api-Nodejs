fdd922fda944f6261f57ddf153c51251c867f3b3
Mustafa Elsayed <sasa6521d@gmail.com>
2023-05-07 15:57:37 +0000

    hot fix

--- src/features/posts/postsRoute.js
+++ src/features/posts/postsRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { createPost, getUserPosts } = require("./postsController");
 const {imageUploader} = require("../../middleware/fileUploadMiddleware");
 
-router.route("/").post(imagesUploader.single("image"),createPost);
+router.route("/").post(imageUploader.single("image"),createPost);
 router.route("/:id").get(getUserPosts);
 
 module.exports = router;
